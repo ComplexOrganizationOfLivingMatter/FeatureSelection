af59d66a2f6d4c4a2be92c1a2f6ea423929a470b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-08-03 17:17:20 +0200

    init getVerticesAndNeighbours

---
+++ SalivaryGland/src/getVerticesAndNeighbours.m
@@ -0,0 +1,16 @@
+function [ vertices ] = getVerticesAndNeighbours( img )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+%
+%   Developed by Pablo Vicente-Munuera and Pedro Gomez-Galvez
+    
+    for row = 1:size(img, 1)
+        for col = 1:size(img, 2)
+            if img(row, col) == 0 %border of polygons
+               if 
+            end
+        end
+    end
+
+end
+
