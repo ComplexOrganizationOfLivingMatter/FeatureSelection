31401da78ff2f1b7aefd979e8cdb9649a73546a9
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-08-05 14:03:20 +0200

    init verify the edges

---
+++ SalivaryGland/src/verifyEdgesBetweenLevels.m
@@ -0,0 +1,12 @@
+function [ edgesBetweenLevelsVerified ] = verifyEdgesBetweenLevels( edgesBetweenLevels )
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+    
+    edgesBetweenLevelsVerified = {};
+
+    %is there interesections?
+    while numEdge < size(edgesBetweenLevels)
+        
+    end
+end
+
