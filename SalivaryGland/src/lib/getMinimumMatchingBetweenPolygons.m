1acfe858b72e8a6a2041caeb2a756057f0ee3890
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-08-08 15:23:26 +0200

    implementing matching vertices

--- SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
+++ SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
@@ -7,52 +7,28 @@ function [ minMatchingEdges ] = getMinimumMatchingBetweenPolygons( centroidsOfVo
     distancePoints = squareform(distancePoints);
     %Don't want the points with themselves
     %Don't want points within the same plane
-    distancePoints(1:size(centroidsOfVoronoiClass,1), 1:size(centroidsOfVoronoiClass,1)) = NaN;
+    distancePoints(1:size(centroidsOfVoronoiClass,1), 1:size(centroidsOfVoronoiNoiseClass,1)) = NaN;
     rowsAux = size(centroidsOfVoronoiClass,1)+1:(size(centroidsOfVoronoiClass,1) + size(centroidsOfVoronoiNoiseClass,1));
     colsAux = size(centroidsOfVoronoiClass,1)+1:(size(centroidsOfVoronoiClass,1) + size(centroidsOfVoronoiNoiseClass,1));
     distancePoints(rowsAux, colsAux) = NaN;
+    distancePoints(rowsAux, :) = NaN;
     
-    combinations = fullfact([size(centroidsOfVoronoiClass, 1), size(centroidsOfVoronoiClass, 1)]);
-    edgesCombinations = {};
-    %Combinations of edges
-    for i = 1:size(combinations, 1); 
-        combinationsAux = combinations;
-        combinationActual = zeros(size(centroidsOfVoronoiClass, 1), 2);
-        numCombinations = 1;
-        row = i;
-        while numCombinations <= size(centroidsOfVoronoiClass, 1)
-           if combinationsAux(row, 1) ~= -1 && combinationsAux(row, 2) ~= -1
-               combinationActual(numCombinations, 1) = combinationsAux(row, 1);
-               combinationActual(numCombinations, 2) = combinationsAux(row, 2) + size(centroidsOfVoronoiClass, 1);
-               combinationsAux(combinationsAux(:, 1) == combinationsAux(row, 1)) = -1;
-               combinationsAux(combinationsAux(:, 2) == combinationsAux(row, 2)) = -1;
-               numCombinations = numCombinations + 1;
-           end
-            row = row + 1;
-            if row > size(combinationsAux, 1)
-               row = 1; 
-            end
-        end
-        edgesCombinations{end+1} = combinationActual;
-    end
-    
-    %Get the minimal combination
-    minMatchingDistance = intmax('int32');
-    for edgesCombination = 1:size(edgesCombinations, 2)
-       combActual = edgesCombinations{edgesCombination};
-       distances = distancePoints(combActual(:, 1), combActual(:, 2));
-       %The diag are the correct values
-       distanceActual = sum(diag(distances));
-       
-       if distanceActual < minMatchingDistance
-           %Conversion of centroids to the correct ones
-           combActual(:, 2) = combActual(:, 2) - size(centroidsOfVoronoiClass, 1);
-           minMatchingEdges = zeros(size(combActual, 1)*2, 3);
-           for i = 1:size(combActual, 1)
-               minMatchingEdges = [minMatchingEdges; centroidsOfVoronoiClass(combActual(i, 1), :), 2; centroidsOfVoronoiNoiseClass(combActual(i, 2), :), 0];
-           end
-           minMatchingDistance = distanceActual
-       end
+    verticesVoronoiAdded = zeros(size(centroidsOfVoronoiClass, 1) , 1);
+    verticesNoiseAdded = zeros(size(centroidsOfVoronoiNoiseClass, 1), 1);
+    minMatchingEdges = [];
+    while min(verticesVoronoiAdded(:,:)) == 0 || min(verticesNoiseAdded(:,:)) == 0
+        minValue = min(distancePoints(:));
+        [rowMin, colMin] = find(distancePoints == minValue, 1);
+        
+        realCol = colMin - size(centroidsOfVoronoiClass, 1);
+        centroidVClass = centroidsOfVoronoiClass(rowMin, :);
+        centroidVNoiseClass = centroidsOfVoronoiNoiseClass(realCol, :);
+        distancePoints(rowMin, colMin) = NaN;
+        
+        verticesVoronoiAdded(rowMin, 1) = 1;
+        verticesNoiseAdded(realCol, 1) = 1;
+        
+        minMatchingEdges = [minMatchingEdges; centroidVClass, 2; centroidVNoiseClass, 0];
     end
 end
 
