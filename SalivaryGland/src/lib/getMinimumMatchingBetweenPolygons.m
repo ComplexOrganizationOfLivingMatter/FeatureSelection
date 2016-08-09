eb07c07ee70a5f8a8eecdc1d934c931a4e8fd4d4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-08-09 10:10:15 +0200

    removing cycles working

--- SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
+++ SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
@@ -28,37 +28,44 @@ function [ minMatchingEdges ] = getMinimumMatchingBetweenPolygons( centroidsOfVo
         verticesVoronoiAdded(rowMin, 1) = verticesVoronoiAdded(rowMin, 1) + 1;
         verticesNoiseAdded(realCol, 1) = verticesNoiseAdded(realCol, 1) + 1;
         
-        minMatchingEdges = [minMatchingEdges; centroidVClass, 2; centroidVNoiseClass, 0];
+        minMatchingEdges = [minMatchingEdges; centroidVClass, 2; centroidVNoiseClass, 0]
     end
     
     %Check if there's useless edges (i.e. linking 2 centroids on each plane
     % several times).
     edgesToRemove = [];
-    %if mod(sum(verticesVoronoiAdded(:, 1) > 0), 2) == 1
-        for edge = 1:size(minMatchingEdges, 1)
-            duplicateEdges = [];
-            %If it's the lower plane, we get the edge of the upper one
-            if mod(edge, 2) == 0
-                duplicateEdges = find(minMatchingEdges(edge, 1) == minMatchingEdges(:, 1) & minMatchingEdges(edge, 2) == minMatchingEdges(:, 2) & minMatchingEdges(edge, 3) == minMatchingEdges(:, 3));
-                duplicateEdges(:) = duplicateEdges(:) + 1;
-            end
-            if size(duplicateEdges, 1) > 1
-                for duplicate = 1:size(duplicateEdges, 1)
-                    consultingEdge = find(minMatchingEdges(duplicateEdges(duplicate), 1) == minMatchingEdges(:, 1) & minMatchingEdges(duplicateEdges(duplicate), 2) == minMatchingEdges(:, 2) & minMatchingEdges(duplicateEdges(duplicate), 3) == minMatchingEdges(:, 3));
-                    if size(consultingEdge, 1) > 1 %there's a cycle, remove the edge between the duplicates
-                        %We only want real edges to erase
-                        if duplicateEdges(duplicate) == edge + 1
-                            %minMatchingEdges (duplicateEdges(duplicate), :) = NaN;
-                            %minMatchingEdges (duplicateEdges(duplicate), :) = NaN;
-                            edgesToRemove = [edgesToRemove; edge; duplicateEdges(duplicate)];
-                        end
+    edge = 1;
+    totalEdges = size(minMatchingEdges, 1);
+    while true
+        duplicateEdges = [];
+        %If it's the lower plane, we get the edge of the upper one
+        if mod(edge, 2) == 1
+            duplicateEdges = find(minMatchingEdges(edge, 1) == minMatchingEdges(:, 1) & minMatchingEdges(edge, 2) == minMatchingEdges(:, 2) & minMatchingEdges(edge, 3) == minMatchingEdges(:, 3));
+            duplicateEdges(:) = duplicateEdges(:) + 1;
+        end
+        if size(duplicateEdges, 1) > 1
+            for duplicate = 1:size(duplicateEdges, 1)
+                consultingEdge = find(minMatchingEdges(duplicateEdges(duplicate), 1) == minMatchingEdges(:, 1) & minMatchingEdges(duplicateEdges(duplicate), 2) == minMatchingEdges(:, 2) & minMatchingEdges(duplicateEdges(duplicate), 3) == minMatchingEdges(:, 3));
+                if size(consultingEdge, 1) > 1 %there's a cycle, remove the edge between the duplicates
+                    %We only want real edges to erase
+                    if duplicateEdges(duplicate) == edge + 1
+                        edge
+                        duplicateEdges(duplicate)
+                        
+                        minMatchingEdges([edge; duplicateEdges(duplicate)], :) = [];
+                        edge = 0;
+                        totalEdges = totalEdges - 2;
+                        break
                     end
                 end
             end
         end
-        edgesToRemove = unique(edgesToRemove, 'rows');
-    %end
+        edge = edge + 1;
+        if edge > totalEdges
+           break 
+        end
+    end
     
-    minMatchingEdges(edgesToRemove,:) = [];
+    
 end
 
