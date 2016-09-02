806164cf57e143eb1adf8e364c515231cad3325f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-02 12:02:28 +0200

    change order of code

--- SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
+++ SalivaryGland/src/lib/getMinimumMatchingBetweenPolygons.m
@@ -32,42 +32,6 @@ function [ minMatchingEdges ] = getMinimumMatchingBetweenPolygons( centroidsOfVo
     %Check if there's useless edges (i.e. linking 2 centroids on each plane
     % several times).
     
-%     totalEdges = size(minMatchingEdges, 1);
-%     distances = zeros(totalEdges/2, 1);
-%     i = 1;
-%     while i <= totalEdges
-%        distances((i+1)/2) = sqrt((minMatchingEdges(i, 1) - minMatchingEdges(i+1, 1))^2 + (minMatchingEdges(i, 2) - minMatchingEdges(i+1, 2))^2);
-%         i = i + 2;
-%     end
-%     
-%     while distances(isnan(distances(:)) == 0) > 0
-%         maxDistance = max(distances(:));
-%         edge = find(maxDistance == distances, 1);
-%         distances(edge) = [];
-%         edge = edge*2 - 1;
-%         duplicateEdges = [];
-%         %If it's the upper plane, we get the edge of the lower one
-%         if mod(edge, 2) == 1
-%             duplicateEdges = find(minMatchingEdges(edge, 1) == minMatchingEdges(:, 1) & minMatchingEdges(edge, 2) == minMatchingEdges(:, 2) & minMatchingEdges(edge, 3) == minMatchingEdges(:, 3));
-%             duplicateEdges(:) = duplicateEdges(:) + 1;
-%         end
-%         if size(duplicateEdges, 1) > 1
-%             for duplicate = 1:size(duplicateEdges, 1)
-%                 consultingEdge = find(minMatchingEdges(duplicateEdges(duplicate), 1) == minMatchingEdges(:, 1) & minMatchingEdges(duplicateEdges(duplicate), 2) == minMatchingEdges(:, 2) & minMatchingEdges(duplicateEdges(duplicate), 3) == minMatchingEdges(:, 3));
-%                 if size(consultingEdge, 1) > 1 %there's a cycle, remove the edge between the duplicates
-%                     %We only want real edges to erase
-%                     if duplicateEdges(duplicate) == edge + 1
-%                         edge
-%                         duplicateEdges(duplicate)
-%                         
-%                         minMatchingEdges([edge; duplicateEdges(duplicate)], :) = [];
-%                         break
-%                     end
-%                 end
-%             end
-%         end
-%     end
-    
     %Pedro's proposal
     p0EdgesUnique = unique(minMatchingEdges(minMatchingEdges(:, 3) == 0, :), 'rows');
     p2EdgesUnique = unique(minMatchingEdges(minMatchingEdges(:, 3) == 6, :), 'rows');
@@ -113,6 +77,44 @@ function [ minMatchingEdges ] = getMinimumMatchingBetweenPolygons( centroidsOfVo
         end
         
     end
+    
+%     totalEdges = size(minMatchingEdges, 1);
+%     distances = zeros(totalEdges/2, 1);
+%     i = 1;
+%     while i <= totalEdges
+%        distances((i+1)/2) = sqrt((minMatchingEdges(i, 1) - minMatchingEdges(i+1, 1))^2 + (minMatchingEdges(i, 2) - minMatchingEdges(i+1, 2))^2);
+%         i = i + 2;
+%     end
+%     
+%     while distances(isnan(distances(:)) == 0) > 0
+%         maxDistance = max(distances(:));
+%         edge = find(maxDistance == distances, 1);
+%         distances(edge) = [];
+%         edge = edge*2 - 1;
+%         duplicateEdges = [];
+%         %If it's the upper plane, we get the edge of the lower one
+%         if mod(edge, 2) == 1
+%             duplicateEdges = find(minMatchingEdges(edge, 1) == minMatchingEdges(:, 1) & minMatchingEdges(edge, 2) == minMatchingEdges(:, 2) & minMatchingEdges(edge, 3) == minMatchingEdges(:, 3));
+%             duplicateEdges(:) = duplicateEdges(:) + 1;
+%         end
+%         if size(duplicateEdges, 1) > 1
+%             for duplicate = 1:size(duplicateEdges, 1)
+%                 consultingEdge = find(minMatchingEdges(duplicateEdges(duplicate), 1) == minMatchingEdges(:, 1) & minMatchingEdges(duplicateEdges(duplicate), 2) == minMatchingEdges(:, 2) & minMatchingEdges(duplicateEdges(duplicate), 3) == minMatchingEdges(:, 3));
+%                 if size(consultingEdge, 1) > 1 %there's a cycle, remove the edge between the duplicates
+%                     %We only want real edges to erase
+%                     if duplicateEdges(duplicate) == edge + 1
+%                         edge
+%                         duplicateEdges(duplicate)
+%                         
+%                         minMatchingEdges([edge; duplicateEdges(duplicate)], :) = [];
+%                         break
+%                     end
+%                 end
+%             end
+%         end
+%     end
+    
+
     
 end
 
