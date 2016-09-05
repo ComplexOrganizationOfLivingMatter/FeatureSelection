fcb65d1ffcc0454fee18af9765fd884c78ec4b70
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-05 11:26:01 +0200

    working with some bugs

--- SalivaryGland/src/paintImageMidPlane.m
+++ SalivaryGland/src/paintImageMidPlane.m
@@ -2,7 +2,31 @@ function [ midPlaneImage ] = paintImageMidPlane(midPlanePoints, edgesMidPlane, v
 %PAINTIMAGEMIDPLANE Summary of this function goes here
 %   Detailed explanation goes here
 
-    zeros(size(voronoiClass, 1), size(voronoiClass, 2));
+    midPlaneImage = zeros(size(voronoiClass, 1), size(voronoiClass, 2));
+    
+    numEdge = 1;
+    while numEdge <= size(edgesMidPlane, 1)
+        point1 = edgesMidPlane(numEdge, :);
+        point2 = edgesMidPlane(numEdge + 1, :);
+        t = diff([point1; point2]);
+        
+        S = strel('line',sqrt(sum(t.^2)),atand(t(1)/-t(2)));
+        imageEdge = S.getnhood;
+        
+        maxHeight = max(point1(1), point2(1));
+        maxWidth = max(point1(2), point2(2));
+        minHeight = min(point1(1), point2(1));
+        minWidth = min(point1(2), point2(2));
+        
+        for i = 1:(maxHeight - minHeight)
+           for j = 1:(maxWidth - minWidth)
+               midPlaneImage(i + minHeight, j + minWidth) = imageEdge(i, j) | midPlaneImage(i + minHeight, j + minWidth);
+           end
+        end
+        
+        
+        numEdge = numEdge + 2;
+    end
 
 end
 
