3150404780f5b574782acca32209a219c3781f93
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-05 10:54:47 +0200

    painting mid plane

---
+++ SalivaryGland/src/paintImageMidPlane.m
@@ -0,0 +1,8 @@
+function [ midPlaneImage ] = paintImageMidPlane(midPlanePoints, edgesMidPlane, voronoiClass)
+%PAINTIMAGEMIDPLANE Summary of this function goes here
+%   Detailed explanation goes here
+
+    zeros(size(voronoiClass, 1), size(voronoiClass, 2));
+
+end
+
