07c760e02e5ce1250dae16cd1c2b0141589cc53f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-06 15:13:55 +0200

    main updated

--- SalivaryGland/main.m
+++ SalivaryGland/main.m
@@ -1,20 +1,16 @@
 %Developed by Pablo Vicente-Munuera and Pedro Gomez-Galvez
 %Both images have the labels and boundaries of cells
-voronoiOriginalAll = importdata('test\image_1_Diagram_1_Vonoroi_out.mat');
-voronoiNoiseOriginalAll = importdata('test\image_1_Diagram_2_Vonoroi_out.mat');
-validClassesOriginal = importdata('test\Valid_cells_image_1.mat');
-
+voronoiOriginalAll = importdata('data\External cylindrical voronoi\Image_2_Diagram_2_Vonoroi_out.mat');
+voronoiNoiseOriginalAll = importdata('data\Inner cylindrical voronoi noise\Whole cell\Image_2_Diagram_3_Vonoroi_noise.mat');
+validClassesOriginal = importdata('data\Valid cells\Whole cell\Valid_cells_image_2.mat');
 
 voronoiClass = repmat(voronoiOriginalAll.L_original, 1, 3);
-voronoiNoise = repmat(voronoiNoiseOriginalAll.L_original, 1, 3);
-
-%xMinImage = min(extremaVoronoi(extremaVoronoi(:,1) ~= 0.5));
-%yMinImage = min(extremaVoronoi(extremaVoronoi(:,2) ~= 0.5));
+voronoiNoise = repmat(voronoiNoiseOriginalAll.L_original_noise, 1, 3);
 
 [verticesV, neighboursVerticesV] = getVerticesAndNeighbours(voronoiClass, voronoiOriginalAll.border_cells);
-[verticesVNoise, neighboursVerticesVNoise] = getVerticesAndNeighbours(voronoiNoise, voronoiNoiseOriginalAll.border_cells);
+[verticesVNoise, neighboursVerticesVNoise] = getVerticesAndNeighbours(voronoiNoise, voronoiNoiseOriginalAll.border_cells_noise);
 
-classesToVisualize = validClassesOriginal.general_valid_cells;
+classesToVisualize = validClassesOriginal.general_valid_noise_whole_cells;
 %classesToVisualize = [66, 67, 70, 77];
 
 [ edgesBetweenLevels, verticesVAdded, verticesVNoiseAdded ] = findingEdgesBetweenLevels(voronoiClass, verticesV, neighboursVerticesV, verticesVNoise, neighboursVerticesVNoise, classesToVisualize);
@@ -29,21 +25,38 @@ edgesMidPlane = remove3Cycle(midPlanePoints, edgesMidPlane);
 
 midPlaneImage = paintImageMidPlane(midPlanePoints, edgesMidPlane, voronoiClass);
 
-%testing
-neighboursMidPlanePoints = cell2mat(neighboursMidPlanePoints)
-figure;
-for classToVisualize = 1:size(classesToVisualize, 2)
-    
-    verticesToVisualize = paintPolyhedron( neighboursVerticesV, verticesV, classesToVisualize, classToVisualize);
-    verticesToVisualize3 = paintPolyhedron( neighboursMidPlanePoints, midPlanePoints, classesToVisualize, classToVisualize);
-    paintAlphaShape(verticesToVisualize3, verticesToVisualize);
-    
-    verticesToVisualize2 = paintPolyhedron( neighboursVerticesVNoise, verticesVNoise, classesToVisualize, classToVisualize);
-    paintAlphaShape(verticesToVisualize3, verticesToVisualize2);
-end
-
-
-
 plottingEpithelialStructure( voronoiClass, voronoiNoise, verticesV, verticesVNoise, edgesBetweenLevels, verticesVAdded, verticesVNoiseAdded, classesToVisualize, midPlanePoints, edgesMidPlane, midPlaneImage);
 
 
+%-------------------- Testing visualizing -----------------------------%
+% neighboursMidPlanePoints = cell2mat(neighboursMidPlanePoints)
+% 
+% topVertices = [];
+% midVertices = [];
+% for classToVisualize = 1:size(classesToVisualize, 2)
+%     [x, ~] = find(neighboursVerticesV == classesToVisualize(classToVisualize));
+%     topVertices = [topVertices; verticesV(x, :)];
+%     [x, ~] = find(neighboursMidPlanePoints == classesToVisualize(classToVisualize));
+%     midVertices = [midVertices; midPlanePoints(x, :)];
+% end
+% 
+% verticesToVisualize = unique([[topVertices, ones(size(topVertices, 1), 1)*6]; midVertices], 'rows');
+% verticesToVisualize(:,3) = verticesToVisualize(:,3) * 100;
+% verticesToVisualize = verticesToVisualize(verticesToVisualize(:, 2) > 512 & verticesToVisualize(:, 2) < 1024, :);
+% figure;
+% paintAlphaShape(verticesToVisualize, [], 1);
+% 
+% %Painting 4 cells
+% figure;
+% for classToVisualize = 1:size(classesToVisualize, 2)
+%     
+%     verticesToVisualize = paintPolyhedron( neighboursVerticesV, [verticesV, ones(size(verticesV, 2), 1)*6], classesToVisualize, classToVisualize);
+%     verticesToVisualize3 = paintPolyhedron( neighboursMidPlanePoints, midPlanePoints, classesToVisualize, classToVisualize);
+%     paintAlphaShape(verticesToVisualize3, verticesToVisualize);
+%     
+%     verticesToVisualize2 = paintPolyhedron( neighboursVerticesVNoise, [verticesVNoise, ones(size(verticesVNoise, 2), 1)*0], classesToVisualize, classToVisualize);
+%     paintAlphaShape(verticesToVisualize3, verticesToVisualize2);
+% end
+
+
+%-------------------- End Testing visualizing -----------------------------%
