b7c88d93291b932e83fe0dba2aa8aa2fefaa5a85
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-06 10:24:57 +0200

    good

--- SalivaryGland/src/lib/paintPolyhedron.m
+++ SalivaryGland/src/lib/paintPolyhedron.m
@@ -3,7 +3,7 @@ function [ verticesToVisualize ] = paintPolyhedron( neighboursVerticesV, vertice
 %   Detailed explanation goes here
     [x, ~] = find(neighboursVerticesV == classesToVisualize(classToVisualize));
     vertices = verticesV(unique(x), :);
-    verticesToVisualize = [vertices, ones(size(vertices, 1), 1)*100];
+    verticesToVisualize(:, 3) = vertices(:, 3) * 100;
     verticesToVisualize = verticesToVisualize(verticesToVisualize(:, 2) > 512 & verticesToVisualize(:, 2) < 1024, :);
 end
 
