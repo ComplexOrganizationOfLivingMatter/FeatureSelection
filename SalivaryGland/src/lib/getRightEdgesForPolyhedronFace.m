08735bacd0924c95aa32806993da9e5ad5966af4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-14 16:26:40 +0200

    not remarkable

--- SalivaryGland/src/lib/getRightEdgesForPolyhedronFace.m
+++ SalivaryGland/src/lib/getRightEdgesForPolyhedronFace.m
@@ -8,18 +8,18 @@ function [ newEdges ] = getRightEdgesForPolyhedronFace( edgesBetweenLevels, vert
     verticesWeWantToVerifyPlane6 = unique(edgesBetweenLevels(verticesPlane6, :), 'rows');
     verticesWeWantToVerifyPlane0 = unique(edgesBetweenLevels(verticesPlane0, :), 'rows');
     
-	if size(verticesWeWantToVerifyPlane6, 1) > 1 && size(verticesWeWantToVerifyPlane0, 1) > 1
+    if (size(verticesWeWantToVerifyPlane6, 1) > 1 && size(verticesWeWantToVerifyPlane0, 1) > 1)
         distanceVertices = pdist([verticesWeWantToVerifyPlane6; verticesWeWantToVerifyPlane0]);
         distanceVertices = squareform(distanceVertices);
-
+        
         %Remove distances between the same plane
         distanceVertices(1:size(verticesWeWantToVerifyPlane6, 1), 1:size(verticesWeWantToVerifyPlane6, 1)) = NaN;
         distanceVertices(size(verticesWeWantToVerifyPlane6, 1)+1:size(verticesWeWantToVerifyPlane6, 1) + size(verticesWeWantToVerifyPlane0, 1), :) = NaN;
-
+        
         newEdges = [];
         combination1 = distanceVertices(1, 3) + distanceVertices(2, 4);
         combination2 = distanceVertices(2, 3) + distanceVertices(1, 4);
-
+        
         cols = [3; 4];
         realCol = cols - size(verticesWeWantToVerifyPlane6, 1);
         if combination1 < combination2
@@ -27,7 +27,7 @@ function [ newEdges ] = getRightEdgesForPolyhedronFace( edgesBetweenLevels, vert
         else
             rows = [2; 1];
         end
-
+        
         newEdges = [verticesWeWantToVerifyPlane6(rows(1), :); verticesWeWantToVerifyPlane0(realCol(1), :)];
         newEdges = [newEdges; verticesWeWantToVerifyPlane6(rows(2), :); verticesWeWantToVerifyPlane0(realCol(2), :)];
     end
