efae8a8b7ce4614eaa2e0a277d110cf552e62d46
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-09-19 12:52:49 +0200

    new function

--- SalivaryGland/main.m
+++ SalivaryGland/main.m
@@ -1,51 +1,24 @@
 %Developed by Pablo Vicente-Munuera and Pedro Gomez-Galvez
-%Both images have the labels and boundaries of cells
-voronoiOriginalAll = importdata('data\External cylindrical voronoi\Image_1_Diagram_3_Vonoroi_out.mat');
-voronoiNoiseOriginalAll = importdata('data\Inner cylindrical voronoi noise\Inside ratio\Image_1_Diagram_3_Vonoroi_noise.mat');
-validClassesOriginal = importdata('data\Valid cells\Inside ratio\Valid_cells_image_1.mat');
 
-disp('Data loaded')
 
-voronoiClass = repmat(voronoiOriginalAll.L_original, 1, 3);
-voronoiNoise = repmat(voronoiNoiseOriginalAll.L_original_noise, 1, 3);
+mypath = 'data\External cylindrical voronoi\';
+filesVoronoi = dir(strcat(mypath, '*.mat'));
 
-%Get vertices and vertices' neighbours of both images
-[verticesV, neighboursVerticesV] = getVerticesAndNeighbours(voronoiClass, voronoiOriginalAll.border_cells);
-[verticesVNoise, neighboursVerticesVNoise] = getVerticesAndNeighbours(voronoiNoise, voronoiNoiseOriginalAll.border_cells_noise);
+for numFileVoronoi = 1:size(filesVoronoi,1)
+    %Both images have the labels and boundaries of cells
+    voronoiOriginalAll = importdata(strcat(mypath, filesVoronoi(numFileVoronoi).name));
+    nameFileSplitted = strsplit(filesVoronoi(numFileVoronoi).name, '_');
+    sharedName = strcat(nameFileSplitted(1:4));
+    sharedName = strcat(sharedName{:})
+    
+    
+    voronoiNoiseOriginalAll = importdata('data\Inner cylindrical voronoi noise\Inside ratio\Image_1_Diagram_6_Vonoroi_noise.mat');
+    validClassesOriginal = importdata('data\Valid cells\Inside ratio\Valid_cells_image_1.mat');
 
-%We only want to visualize the valid cells
-%classesToVisualize = validClassesOriginal.general_valid_noise_inner_ratio_cells;
-classesToVisualize = validClassesOriginal.general_valid_noise_whole_cells;
-%You have to delete the first number
-borderCells = union(voronoiOriginalAll.border_cells(2:end), voronoiNoiseOriginalAll.border_cells_noise(2:end));
-%classesToVisualize = [63, 55];
-
-disp('Vertices found')
-
-%Create an edge between both voronoi images: VoronoiClass and VoronoiNoise
-[ edgesBetweenLevels, verticesVAdded, verticesVNoiseAdded ] = findingEdgesBetweenLevels(voronoiClass, verticesV, neighboursVerticesV, verticesVNoise, neighboursVerticesVNoise, classesToVisualize, borderCells);
-
-disp('Edges between levels... Verifying')
-
-%Remove unwanted (not good) vertices between planes(or levels)
-[ edgesBetweenLevels ] = verifyEdgesBetweenLevels(edgesBetweenLevels, voronoiClass, neighboursVerticesV, verticesV, neighboursVerticesVNoise, verticesVNoise, classesToVisualize, borderCells);
-
-%Find the points that create an X in the mid plane, i.e. the so call T1 transitions
-[t1Points, edgesBetweenLevels] = gettingT1Transitions(edgesBetweenLevels);
-
-disp('Mid plane...')
-
-%Get all the points of the mid plane, which will be the middle of the edges between labels.
-[ midPlanePoints, neighboursMidPlanePoints, edgesMidPlane ]  = getIntersectingPlane(edgesBetweenLevels, verticesV, verticesVNoise, neighboursVerticesV, neighboursVerticesVNoise, voronoiClass);
-
-%Remove mistaken edges
-edgesMidPlane = remove3Cycle(midPlanePoints, edgesMidPlane);
-
-%Paint the mid image with the proper classes for the new cells (mid plane)
-midPlaneImage = paintImageMidPlane(midPlanePoints, edgesMidPlane, voronoiClass);
-%Plot all the information
-disp('Plotting...')
-plottingEpithelialStructure( voronoiClass, voronoiNoise, verticesV, verticesVNoise, edgesBetweenLevels, verticesVAdded, verticesVNoiseAdded, classesToVisualize, midPlanePoints, edgesMidPlane, midPlaneImage);
+    disp('Data loaded')
+    
+    [ edgesBetweenLevels, t1Points, edgesMidPlane, midPlaneImage] = intersecting3DCellStructure(voronoiOriginalAll, voronoiNoiseOriginalAll, validClassesOriginal)
+end
 
 
 %-------------------- Testing visualizing -----------------------------%
