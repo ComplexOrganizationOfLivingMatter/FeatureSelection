abaace7dc15b7fa2effaf95dafc97c71b378cd17
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-14 11:55:52 +0200

    network from voronoi diagram working

--- voronoiNetworks/src/createNetworksFromVoronoiDiagrams.m
+++ voronoiNetworks/src/createNetworksFromVoronoiDiagrams.m
@@ -1,24 +1,29 @@
 function [ ] = createNetworksFromVoronoiDiagrams( )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    dataFiles = getAllFiles('data\voronoiDiagrams\');
+    dataFiles = getAllFiles('E:\Pablo\PhD-miscelanious\voronoiNetworks\data\voronoiDiagrams\');
     for numFile = 1:size(dataFiles,1)
         fullPathFile = dataFiles(numFile);
         fullPathFile = fullPathFile{:};
         diagramName = strsplit(fullPathFile, '\');
-        diagramName = diagramName(10);
+        diagramName = diagramName(end);
+        diagramName = diagramName{1};
 
         %Check which files we want.
         if size(strfind(lower(diagramName), '.mat'), 1) >= 1
             load(fullPathFile);
-
+            
+            %celulas_validas = validCells
             validCellNeighbours = Vecinos(celulas_validas);
-            adjacencyMatrix = zeros(size(validCellNeighbours, 2));
+            adjacencyMatrix = zeros(size(Vecinos, 2));
             for numCell = 1:size(validCellNeighbours, 2)
-                adjacencyMatrix(numCell, validCellNeighbours(numCell)) = 1;
+                neighbours = validCellNeighbours(numCell);
+                adjacencyMatrix(celulas_validas(numCell), neighbours{1}) = 1;
             end
             outputFile = strcat('results\networks\adjacencyMatrix', diagramName);
             save(outputFile, 'adjacencyMatrix');
+            
+            generateLEDAFromAdjacencyMatrix
         end
     end
 
