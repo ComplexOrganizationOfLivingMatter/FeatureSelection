6d2c2e0d5a297a1fac729a1e8c2e9b0a91ad2c8a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-19 11:04:23 +0200

    files by pedro

--- voronoiNetworks/src/lib/ByPedro/Calculate_neighbors_polygon_distribution.m
+++ voronoiNetworks/src/lib/ByPedro/Calculate_neighbors_polygon_distribution.m
@@ -40,9 +40,62 @@ function Calculate_neighbors_polygon_distribution(currentPath)
             
             nameWithoutExtension = strsplit(diagramName, '.');
             outputFile = strcat(strjoin(fullPathSplitted(1:end-2), '\'), '\data\', nameWithoutExtension{1}, '_data.mat');
-            load(outputFile);
+            %load(outputFile);
+            
+            %Removing borderCells
+            H = size(image, 1);
+            W = size(image, 2);
+            Img_det_bord=bwlabel(image,8);
+            Img_det_bord(1,1:W)=1;
+            Img_det_bord(H,1:W)=1;
+            Img_det_bord(1:H,1)=1;
+            Img_det_bord(1:H,W)=1;
+            Img_det_bord = bwlabel(Img_det_bord,8);
+            borderCells=[];
+            noBorderCells=[];
+            
+            %-- OMM files (eye) --%
+%             borderCells = [1, neighbours{1}'];
+%             allCells = 1:max(Img_L(:));
+%             noBorderCells = ismember(allCells, borderCells);
+%             noBorderCells = allCells(noBorderCells == 0);
+            %--Not OMM (eye) files --%
+            for cell=2:max(Img_L(:))
+                if unique(Img_det_bord(Img_L == cell))~=1
+                    noBorderCells = [noBorderCells, cell];
+                else
+                    borderCells = [borderCells, cell];
+                end
+            end
+            
+            % Removing cells isolated from the valid cells
+            noValidCells = unique(borderCells);
+            validCells = unique(noBorderCells);
+            vecinos_real = neighbours;
+            
+            celulas_validas_previa = validCells;
+            celulas_no_validas_previa = noValidCells;
+            flag = 0;
+            for j=1:length(celulas_validas_previa) % Bucle que recorre todas celulas validas para comprobar
+                j;
+                vec_cel_ind_j=vecinos_real{celulas_validas_previa(j)};
+                no_coincidencia=[];
+                for x=1:length(vec_cel_ind_j)% Bucle que recorre todos los vecinos de la celula bajo estudio
+                    no_coincidencia(x)=isempty(find(vec_cel_ind_j(x)==celulas_no_validas_previa)); %Variable que vale 1 si no existe coincidencia de la celula j con alguna de las celulas no validas
+                end
+                if sum(no_coincidencia)==0
+                    validCells=validCells(validCells~=celulas_validas_previa(j));
+                    noValidCells=sort([celulas_validas_previa(j),celulas_no_validas_previa]);
+                    flag=1;
+                end
+            end
+            if (flag==0)
+                validCells=celulas_validas_previa;                                        %%VARIABLE A GUARDAR
+                noValidCells=celulas_no_validas_previa;
+            end
             
             vecinos = neighbours;
+            celulas_validas = validCells;
             save(outputFile, 'vecinos', 'celulas_validas');
         end
     end
