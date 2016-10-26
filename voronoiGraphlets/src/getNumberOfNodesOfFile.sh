49f4c580e46c27301167c735f3a40fdf733b1a9c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-10-26 11:12:34 +0200

    in a command -_-

--- voronoiGraphlets/src/getNumberOfNodesOfFile.sh
+++ voronoiGraphlets/src/getNumberOfNodesOfFile.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-ls | while read f; do cat "$f, " >> "numNodes$1.csv;" wc -l "$f" >> "numNodes$1.csv"; cat "\n" >> "numNodes$1.csv"; done
+ls | while read f; do wc -l $f >> "numNodes$1.csv"; done
