84493f658c7bcfdb0824bc2a104e76f6ce2a914a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-11-15 14:12:26 +0100

    added against voronoi1

--- voronoiGraphlets/src/analyzeGraphletDistances.m
+++ voronoiGraphlets/src/analyzeGraphletDistances.m
@@ -7,12 +7,20 @@ function [ ] = analyzeGraphletDistances(currentPath, typeOfDistance)
     graphletName = graphletNameSplitted(end);
     graphletName = graphletName{1};
 
-    distanceMatrix = dlmread(fullPathGraphlet, '\t', 1, 1);
+    distanceMatrixInit = dlmread(fullPathGraphlet, '\t', 1, 1);
     names = importfileNames(fullPathGraphlet);
     [names, it] = sort(names);
-    distanceMatrix = distanceMatrix(it, it);
+    distanceMatrix = distanceMatrixInit(it, it);
     
-
     save(strcat(currentPath, 'distanceMatrix', upper(typeOfDistance) ,'.mat'), 'distanceMatrix', 'names');
+    
+    voronoiDiagram1 = cellfun(@(x) isempty(strfind(x, 'Diagrama_01')) == 0 & isempty(strfind(x, 'weight')), names);
+    distanceMatrix = distanceMatrix(voronoiDiagram1 == 0, voronoiDiagram1);
+    distanceMatrix = distanceMatrix(2:end, :);
+    differenceMean = mean(distanceMatrix, 2);
+    names = names(voronoiDiagram1 == 0);
+    names = names(2:end);
+    
+    save(strcat(currentPath, 'distanceMatrixVoronoi1', upper(typeOfDistance) ,'.mat'), 'distanceMatrix', 'names', 'differenceMean');
 end
 
