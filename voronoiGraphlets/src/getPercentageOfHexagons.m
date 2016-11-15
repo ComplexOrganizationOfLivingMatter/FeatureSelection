ebcc3cdf0abd11d7f643ca62cd1d14d7a870d7e9
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-11-15 14:02:01 +0100

    generalize

--- voronoiGraphlets/src/getPercentageOfHexagons.m
+++ voronoiGraphlets/src/getPercentageOfHexagons.m
@@ -1,4 +1,4 @@
-function [  ] = getPercentageOfHexagons( currentPath )
+function [  ] = getPercentageOfHexagons( currentPath, maxLengthStr )
 %GETPERCENTAGEOFHEXAGONS Get the percentage of hexagons of a tesselation.
 %   Calculate the number of nodes surrounded by 6 neighbours.
 %   This number will be divided by the total number of nodes, 
@@ -12,26 +12,54 @@ function [  ] = getPercentageOfHexagons( currentPath )
     totalGraphlets = getAllFiles(currentPath);
 
     percentageOfHexagons = [];
+    percentageOfTriangles = [];
+    percentageOfSquares = [];
+    percentageOfHeptagons = [];
+    percentageOfPentagons = [];
+    percentageOfOctogons = [];
+    percentageOfNonagons = [];
+    percentageOfDecagons = [];
+    
     nameFiles = {};
     for numFile = 1:size(totalGraphlets, 1)
         fullPathFile = totalGraphlets(numFile);
         fullPathFile = fullPathFile{:};
-        diagramName = strsplit(fullPathFile, '\');
-        diagramName = diagramName(end);
+        diagramNameSplitted = strsplit(fullPathFile, '\');
+        diagramName = diagramNameSplitted(end);
         diagramName = diagramName{1};
 
         %Check which files we want.
-        if isempty(strfind(lower(diagramName), '.ndump2')) == 0
-            matrixGraphlets = csvread(fullPathFile);
-            percentageOfHexagons(end+1) = sum(matrixGraphlets(:, 1) == 6) / size(matrixGraphlets, 1)*100;
-            nameFiles{end+1} = fullPathFile;
+        if isempty(strfind(lower(diagramName), '.ndump2')) == 0 && isempty(strfind(lower(diagramNameSplitted{end-1}), 'atrophicCells')) && isempty(strfind(lower(diagramNameSplitted{end-1}), 'Cancer'))
+            if isempty(strfind(lower(diagramNameSplitted{4}), lower(maxLengthStr))) == 0 || isempty(maxLengthStr)
+                matrixGraphlets = csvread(fullPathFile);
+
+                percentageOfTriangles(end+1) = sum(matrixGraphlets(:, 1) == 3) / size(matrixGraphlets, 1)*100;
+                percentageOfSquares(end+1) = sum(matrixGraphlets(:, 1) == 4) / size(matrixGraphlets, 1)*100;
+                percentageOfPentagons(end+1) = sum(matrixGraphlets(:, 1) == 5) / size(matrixGraphlets, 1)*100;
+                percentageOfHexagons(end+1) = sum(matrixGraphlets(:, 1) == 6) / size(matrixGraphlets, 1)*100;
+                percentageOfHeptagons(end+1) = sum(matrixGraphlets(:, 1) == 7) / size(matrixGraphlets, 1)*100;
+                percentageOfOctogons(end+1) = sum(matrixGraphlets(:, 1) == 8) / size(matrixGraphlets, 1)*100;
+                percentageOfNonagons(end+1) = sum(matrixGraphlets(:, 1) == 9) / size(matrixGraphlets, 1)*100;
+                percentageOfDecagons(end+1) = sum(matrixGraphlets(:, 1) == 10) / size(matrixGraphlets, 1)*100;
+                nameFiles{end+1} = fullPathFile;
+            end
         end
     end
-
-    percentageOfHexagons;
-    nameFiles;
-
-    save('percentageOfHexagons.mat', 'percentageOfHexagons', 'nameFiles');
+    %missing:  
+    characteristicsCVT = [percentageOfTriangles; percentageOfSquares; percentageOfPentagons; percentageOfHexagons; percentageOfHeptagons; percentageOfOctogons; percentageOfNonagons; percentageOfDecagons]';
+    distanceMatrix = squareform(pdist(characteristicsCVT));
+%     points = mdscale(distanceMatrix, 2);
+%     
+%     nameFilesSplitted = cellfun(@(x) strsplit(x, '_'), nameFiles, 'UniformOutput', false);
+%     nameFilesFinal = cellfun(@(x) x(5), nameFilesSplitted);
+%     filter15 = cellfun(@(x) isequal(x, '015'), nameFilesFinal);
+%     figure; plot(points(:, 1), points(:, 2), 'o');
+%     t1 = text(points(filter15, 1), points(filter15, 2), nameFilesFinal(filter15), 'FontSize', 5, 'HorizontalAlignment', 'center');
+%     percentageOfHexagons;
+%     nameFiles;
+    %points1Dimension = mdscale(distanceMatrix, 1, 'Criterion', 'sstress');
+    save(strcat('results\comparisons\EveryFile\polygonDistributionDistanceMatrix', maxLengthStr ,'.mat'), 'distanceMatrix', 'nameFiles');
+    save(strcat('results\comparisons\EveryFile\percentageOfHexagons', maxLengthStr, '.mat'), 'percentageOfHexagons', 'nameFiles');
 
 end
 
