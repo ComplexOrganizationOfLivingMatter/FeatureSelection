a7a55800e698f90bddfd8bc9ee40713b94ac34d8
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-11-24 14:47:50 +0100

    new data

--- voronoiGraphlets/src/createTotalGraphlets.sh
+++ voronoiGraphlets/src/createTotalGraphlets.sh
@@ -1,29 +1,50 @@
 #Original
-# ls | grep dWL | while read f; do echo "$f"; cat "$f" >> dWL_totalGraphlets.ndump2; done;
-# ls | grep dWP | while read f; do echo "$f"; cat "$f" >> dWP_totalGraphlets.ndump2; done;
-# ls | grep cNT | while read f; do echo "$f"; cat "$f" >> cNT_totalGraphlets.ndump2; done;
-# ls | grep omm | while read f; do echo "$f"; cat "$f" >> eye_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_20_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_20_totalGraphlets.ndump2; done;
-# ls | grep BC | while read f; do echo "$f"; cat "$f" >> BCA_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_01_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_01_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_02_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_02_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_03_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_03_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_04_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_04_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_05_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_05_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_06_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_06_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_07_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_07_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_08_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_08_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_09_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_09_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_10_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_10_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_11_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_11_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_12_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_12_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_13_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_13_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_14_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_14_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_15_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_15_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_16_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_16_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_17_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_17_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_18_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_18_totalGraphlets.ndump2; done;
-# ls | grep Diagrama_19_data.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoi_19_totalGraphlets.ndump2; done;
+ls | grep dWL | while read f; do echo "$f"; cat "$f" >> dWL_totalGraphlets.ndump2; done;
+ls | grep dWP | while read f; do echo "$f"; cat "$f" >> dWP_totalGraphlets.ndump2; done;
+ls | grep cNT | while read f; do echo "$f"; cat "$f" >> cNT_totalGraphlets.ndump2; done;
+ls | grep omm | while read f; do echo "$f"; cat "$f" >> omm_totalGraphlets.ndump2; done;
+ls | grep BC | while read f; do echo "$f"; cat "$f" >> BCA_totalGraphlets.ndump2; done;
+ls | grep Diagrama_001.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_001_totalGraphlets.ndump2; done;
+ls | grep Diagrama_002.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_002_totalGraphlets.ndump2; done;
+ls | grep Diagrama_003.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_003_totalGraphlets.ndump2; done;
+ls | grep Diagrama_004.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_004_totalGraphlets.ndump2; done;
+ls | grep Diagrama_005.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_005_totalGraphlets.ndump2; done;
+ls | grep Diagrama_006.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_006_totalGraphlets.ndump2; done;
+ls | grep Diagrama_007.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_007_totalGraphlets.ndump2; done;
+ls | grep Diagrama_008.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_008_totalGraphlets.ndump2; done;
+ls | grep Diagrama_009.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_009_totalGraphlets.ndump2; done;
+ls | grep Diagrama_010.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_010_totalGraphlets.ndump2; done;
+ls | grep Diagrama_011.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_011_totalGraphlets.ndump2; done;
+ls | grep Diagrama_012.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_012_totalGraphlets.ndump2; done;
+ls | grep Diagrama_013.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_013_totalGraphlets.ndump2; done;
+ls | grep Diagrama_014.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_014_totalGraphlets.ndump2; done;
+ls | grep Diagrama_015.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_015_totalGraphlets.ndump2; done;
+ls | grep Diagrama_016.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_016_totalGraphlets.ndump2; done;
+ls | grep Diagrama_017.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_017_totalGraphlets.ndump2; done;
+ls | grep Diagrama_018.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_018_totalGraphlets.ndump2; done;
+ls | grep Diagrama_019.ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_019_totalGraphlets.ndump2; done;
+
+for i in { 20 30 40 50 60 70 80 90 }
+do
+	ls | grep Diagrama_0"$i".ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_0"$i"_totalGraphlets.ndump2; done;
+done
+
+for i in { 100 200 300 400 500 600 700 }
+do
+	ls | grep Diagrama_"$i".ndump2 | while read f; do echo "$f"; cat "$f" >> imagen_Diagrama_"$i"_totalGraphlets.ndump2; done;
+done
+
+#------------------- SickEpithliums --------------#
+
+ls | grep Atrophy- | while read f; do echo "$f"; cat "$f" >> Atrophy_totalGraphlets.ndump2; done;
+ls | grep Case-II- | while read f; do echo "$f"; cat "$f" >> Case-II_totalGraphlets.ndump2; done;
+ls | grep Case-III- | while read f; do echo "$f"; cat "$f" >> Case-III_totalGraphlets.ndump2; done;
+ls | grep Case-IV- | while read f; do echo "$f"; cat "$f" >> Case-IV_totalGraphlets.ndump2; done;
+ls | grep Control-Sim-no-Prol- | while read f; do echo "$f"; cat "$f" >> Control-Sim-no-Prol_totalGraphlets.ndump2; done;
+ls | grep Control-Sim-Prolif- | while read f; do echo "$f"; cat "$f" >> Control-Sim-Prolif_totalGraphlets.ndump2; done;
+ls | grep dMWP | while read f; do echo "$f"; cat "$f" >> dMWP_totalGraphlets.ndump2; done;
+
+
 #------------------- Cancer ----------------------#
 #Columns
 # for i in {1..9}
@@ -40,19 +61,35 @@
 
 
 # #Disk
-# for i in {1..9}
-# do
-# 	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_500_1000_2000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_0"$i"_weight_500_1000_2000_totalGraphlets.ndump2; done;
-# 	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_1000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_0"$i"_weight_1000_totalGraphlets.ndump2; done;
-# 	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_2000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_0"$i"_weight_2000_totalGraphlets.ndump2; done;
-# done
+for i in {1..9}
+do
+	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_2000_OnlyNeighboursOfWeightedCells | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_0"$i"_weight_2000_OnlyNeighboursOfWeightedCells_totalGraphlets.ndump2; done;
+done
 
-# for i in {10..80}
-# do
-# 	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_500_1000_2000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_"$i"_weight_500_1000_2000_totalGraphlets.ndump2; done;
-# 	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_1000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_"$i"_weight_1000_totalGraphlets.ndump2; done;
-# 	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_2000 | while read f; do echo "$f"; cat "$f" >> voronoi_disk_"$i"_weight_2000_totalGraphlets.ndump2; done;
-# done
+for i in {10..80}
+do
+	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_2000_OnlyNeighboursOfWeightedCells | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_"$i"_weight_2000_OnlyNeighboursOfWeightedCells_totalGraphlets.ndump2; done;
+done
+
+for i in {1..9}
+do
+	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_2000_OnlyWeightedCells | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_0"$i"_weight_2000_OnlyWeightedCells_totalGraphlets.ndump2; done;
+done
+
+for i in {10..80}
+do
+	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_2000_OnlyWeightedCells | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_"$i"_weight_2000_OnlyWeightedCells_totalGraphlets.ndump2; done;
+done
+
+for i in {1..9}
+do
+	ls | grep _disk_ | grep Diagrama_0"$i"_v3_weight_2000.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_0"$i"_weight_2000_totalGraphlets.ndump2; done;
+done
+
+for i in {10..80}
+do
+	ls | grep _disk_ | grep Diagrama_"$i"_v3_weight_2000.ndump2 | while read f; do echo "$f"; cat "$f" >> voronoiWeighted_disk_"$i"_weight_2000_totalGraphlets.ndump2; done;
+done
 
 # #Half
 # for i in {1..9}
@@ -82,7 +119,26 @@
 # 	ls | grep _square_ | grep Diagrama_"$i"_v3_weight_800 | while read f; do echo "$f"; cat "$f" >> voronoi_square_"$i"_weight_800_totalGraphlets.ndump2; done;
 # done
 
+#------------------- voronoi noise -------------------#
+
 for i in {1..9}
 do
-	ls | grep _"$i".00_ | while read f; do mv "$f" GoodOnes/"$f"; done;
+	ls | grep Voronoi_00"$i"_ | while read f; do echo "$f"; cat "$f" >> voronoiNoise_00"$i"_totalGraphlets.ndump2; done;
+done
+
+for i in {10..20} 
+do
+	ls | grep Voronoi_0"$i"_ | while read f; do echo "$f"; cat "$f" >> voronoiNoise_0"$i"_totalGraphlets.ndump2; done;
+done
+
+for i in { 30 40 50 60 70 80 90 } 
+do
+	ls | grep Voronoi_0"$i"_ | while read f; do echo "$f"; cat "$f" >> voronoiNoise_0"$i"_totalGraphlets.ndump2; done;
+done
+
+
+
+for i in { 100 200 300 400 500 600 700 } 
+do
+	ls | grep Voronoi_"$i"_ | while read f; do echo "$f"; cat "$f" >> voronoiNoise_"$i"_totalGraphlets.ndump2; done;
 done
