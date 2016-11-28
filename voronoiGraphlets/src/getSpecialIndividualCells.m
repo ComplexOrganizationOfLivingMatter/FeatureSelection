0ae92ace02231507a0de78c332168eb766f77aaa
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-11-28 14:56:43 +0100

    init individual cells

--- voronoiGraphlets/src/getSpecialIndividualCells.m
+++ voronoiGraphlets/src/getSpecialIndividualCells.m
@@ -1,7 +1,22 @@
-function [ ] = getSpecialIndividualCells( currentPath, )
+function [ ] = getSpecialIndividualCells( infoPath, outputPath, additionalDataPath)
 %GETSPECIALINDIVIDUALCELLS Summary of this function goes here
 %   Detailed explanation goes here
 
-
+    allFilesInfo = getAllFiles(infoPath);
+    allFilesData = getAllFiles(additionalDataPath);
+    pathSplitted = strsplit(additionalDataPath, '\');
+    typeOfData = pathSplitted{end-1};
+    for numFile = 1:size(allFilesData,1)
+        fullPathFile = allFilesData{numFile};
+        fullPathFileSplitted = strsplit(fullPathFile, '\');
+        fileName = fullPathFileSplitted{end};
+        
+        if isempty(strfind(fileName, '_data.mat')) %Only analyze additional information
+            load(fileName);
+            fileNameSplitted = strsplit(fileName, '_');
+            
+            graphletFile = cellfun(@(x) isempty(strfind(x, fileNameSplitted(1:2))) == 0, allFilesInfo);
+        end
+    end
 end
 
