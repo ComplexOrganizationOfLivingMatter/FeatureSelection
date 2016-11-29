9563cb7471f50bbd827d09394f67938b02667708
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-11-29 10:17:19 +0100

    removing project

--- voronoiGraphlets/src/distanceMatrixToPoints.R
+++
@@ -1,6 +0,0 @@
-library("R.matlab", lib.loc="~/R/win-library/3.3")
-matFile = readMat('../../PhD-miscelanious/voronoiGraphlets/results/comparisons/EveryFile/maxLength5/polygonDistributionDistanceMatrix.mat')
-distanceMatrix <- matFile$distanceMatrix
-points <- cmdscale(distanceMatrix, 2)
-points1Dimension <- points[, 1]
-writeMat('../../PhD-miscelanious/voronoiGraphlets/results/comparisons/EveryFile/maxLength5/polygonDistribution1DimensionMaxLength5.mat', points=points, points1Dimension=points1Dimension)
--- voronoiGraphlets/src/getNumberOfNodesOfFile.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-ls | while read f; do wc -l $f >> "numNodes$1.csv"; done
