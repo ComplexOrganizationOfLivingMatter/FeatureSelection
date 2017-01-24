04de50e58b8ec082b20b1720b23220ecbc6c20d6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-01-24 16:52:31 +0100

    exctract method

--- DNA-Damage/pipeline.m
+++ DNA-Damage/pipeline.m
@@ -5,55 +5,8 @@ function [] = pipeline( )
 %   Developed by Pablo Vicente-Munuera
 
     cd 'E:\Pablo\PhD-miscelanious\DNA-Damage\'
-    allImages = getAllFiles('data/images/');
-
-    %A big number because of the final if of the for
-    previousNumChannel = 20;
-
-    imagesOfSerieByChannel = {};
-    numChannel = 0;
-    for fileIndex = 1:size(allImages, 1)
-        fullPathImage = allImages{fileIndex};
-        fileNameSplitted = strsplit(fullPathImage, '\');
-        fileName = fileNameSplitted{end};
-        dirName = fileNameSplitted{end-1};
-
-        img = imread(fullPathImage);
-        img = im2double(img);
-
-        %Select the channel
-        if isempty(strfind(lower(fileName), lower('ch00'))) == 0
-            numChannel = 1;
-        elseif isempty(strfind(lower(fileName), lower('ch01'))) == 0
-            numChannel = 2;
-        end
-
-        if previousNumChannel < numChannel
-            imagesOfSerieByChannel{end, numChannel} = img(:,:, numChannel+1); %green
-        else
-            imagesOfSerieByChannel{end+1, numChannel} = img(:,:, numChannel+1); %blue
-        end
-        previousNumChannel = numChannel;
+    extractImages();
     
-
-        if fileIndex < size(allImages, 1)
-            fullPathNextSerie = allImages{fileIndex+1};
-            fileNameSplitted = strsplit(fullPathNextSerie, '\');
-            nextSerie = fileNameSplitted{end};
-            nextSerie = nextSerie(1:9);
-        else
-            nextSerie = '';
-        end
-        
-        %Check if it's another Serie of images
-        if isequal(nextSerie, fileName(1:9)) == 0
-            outputDir = strcat('results\', dirName);
-            mkdir(outputDir);
-            strcat(outputDir, '\', fileName(1:9))
-            save(strcat(outputDir, '\', fileName(1:9)), 'imagesOfSerieByChannel');
-            imagesOfSerieByChannel = {};
-            previousNumChannel = 20;
-        end
-    end
+    
 end
 
