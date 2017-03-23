e6bf9a693b53cb955a1466cb9653b185ab7860b3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-23 14:43:31 +0100

    parallel

--- t-sneAdaptation/main.m
+++ t-sneAdaptation/main.m
@@ -21,27 +21,7 @@ TSNEClassification(G93A_100, G93A_120, 'G93A_100', 'G93A_120');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%Original
+%% Original
 PCA_2_cc_Original(WT_120, CONT_120, 'WT_120', 'CONT_120');
 PCA_2_cc_Original(WT_100, CONT_100, 'WT_100', 'CONT_100');
 
@@ -56,7 +36,7 @@ PCA_2_cc_Original(CONT_100, CONT_120, 'CONT_100', 'CONT_120');
 PCA_2_cc_Original(G93A_100, G93A_120, 'G93A_100', 'G93A_120');
 
 
-%----- Perro ---------%
+%% ----- Perro ---------%
 
 matrices = load('data/perro')
 matrices=struct2cell(matrices);
@@ -69,26 +49,31 @@ parfor i=2:size(names,2)
 
 end
 
-%--- NB ---%
+%% --- NB ---%
 %uiopen('D:\Pablo\Neuroblastoma\Results\graphletsCount\NuevosCasos\Analysis\Characteristics_GDDA_AgainstControl_RiskWithWeights_24_02_2017.csv',1)
 %---------- INSTABILITY ---------------%
 matrix2 = CharacteristicsGDDAAgainstControlInstabilityGDDUsingWeights1303;
 matrixChar = table2array(matrix2(:, 3:end));
 matrixChar(matrixChar(:, :) == -1) = 0;
 
-class2 = cellfun(@(x) isequal('Alta', x), matrix2.Inestabilidad);
-PCA_2_cc_Original(matrixChar(class2==0, :), matrixChar(class2, :), 'Rest', 'High', 0);
-PCA_2_cc_Original(matrixChar(class2==0, :), matrixChar(class2, :), 'Rest', 'High', 1);
+class1 = cellfun(@(x) isequal('Alta', x), matrix2.Inestabilidad);
 class4 = cellfun(@(x) isequal('Muy baja', x), matrix2.Inestabilidad);
-PCA_2_cc_Original(matrixChar(class4, :), matrixChar(class2, :), 'VeryLow', 'High', 0);
-PCA_2_cc_Original(matrixChar(class4, :), matrixChar(class2, :), 'VeryLow', 'High', 1);
+PCA_2_cc_Original(matrixChar(class4, :), matrixChar(class1, :), 'VeryLow', 'High', 0);
+%PCA_2_cc_Original(matrixChar(class4, :), matrixChar(class2, :), 'VeryLow', 'High', 1);
 class3 = cellfun(@(x) isequal('Baja', x), matrix2.Inestabilidad);
 PCA_2_cc_Original(matrixChar(class4, :), matrixChar(class3, :), 'VeryLow', 'Low', 0);
 
-%------------- Risk ---------------------%
-matrix2 = CharacteristicsGDDAAgainstControlRiskGDDUsingWeights13032017;
-matrixChar = table2array(matrix2(:, 3:end));
-matrixChar(matrixChar(:, :) == -1) = 0;
-class1 = cellfun(@(x) isequal('NoRisk', x), matrix2.Risk);
-PCA_2_cc_Original(matrixChar(class1, :), matrixChar(class1==0, :), 'NoRisk', 'HighRisk', 0);
-PCA_2_cc_Original(matrixChar(class1, :), matrixChar(class1==0, :), 'NoRisk', 'HighRisk', 1);
+classifications(1, :) = {CharacteristicsGDDAAgainstControlInstabilityGDDUsingWeights1303, 'Alta', 'Rest', 'High'};
+classifications(2, :) = {CharacteristicsGDDAAgainstControlRiskGDDUsingWeights13032017, 'NoRisk', 'NoRisk', 'HighRisk'};
+classifications(3, :) = {CharacteristicsGDDAAgainstControlAge22032017, 'LowAge', 'LowAge', 'GreaterAge'};
+classifications(4, :) = {CharacteristicsGDDAAgainstControlStage22032017, 'EarlyStage', 'EarlyStage', 'LateStage'};
+classifications(5, :) = {CharacteristicsGDDAAgainstControlAP22032017, 'BetterAP', 'BetterAP', 'WorseAP'};
+classifications(6, :) = {CharacteristicsGDDAAgainstControlNeupat22032017, 'BetterNeupat', 'BetterNeupat', 'WorseNeupat'};
+
+parfor i = 1:size(classifications, 2)
+    matrix2 = classifications{i, 1};
+    matrixChar = table2array(matrix2(:, 3:end));
+    matrixChar(matrixChar(:, :) == -1) = 0;
+    class1 = cellfun(@(x) isequal(classifications{i, 2}, x), matrix2{:, 2});
+    PCA_2_cc_Original(matrixChar(class1, :), matrixChar(class1==0, :), classifications{i, 3}, classifications{i, 4}, 0);
+end
