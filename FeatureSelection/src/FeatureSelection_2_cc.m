5946987fcff109fa84670a099b0daca7f2e449f4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-28 16:19:12 +0200

    adding date to the name

--- FeatureSelection/src/FeatureSelection_2_cc.m
+++ FeatureSelection/src/FeatureSelection_2_cc.m
@@ -134,11 +134,11 @@ function FeatureSelection_2_cc(matrix_t1, matrix_t2, name_t1, name_t2, usedMetho
      [ sensitivity, specifity, classificationResult, AUC, VPpositive, VPnegative] = getSensitivityAndSpecifity( nImgType1, name_t1, n_images, name_t2, Proyecc);
     
     mkdir('results');
-    save( ['results\' lower(usedMethod) 'FeatureSelection_' name_t1 '_' name_t2 '_selection_cc_' num2str(n_totalCcs)], 'BettersPCAEachStep', 'Proy', 'bestPCA','indicesCcsSelected', 'weightsOfCharacteristics', 'sensitivity', 'specifity', 'classifierResult', 'AUC', 'VPpositive', 'VPnegative');
+    save( ['results\' lower(usedMethod) 'FeatureSelection_' name_t1 '_' name_t2 '_selection_cc_' num2str(n_totalCcs) '_' date ], 'BettersPCAEachStep', 'Proy', 'bestPCA','indicesCcsSelected', 'weightsOfCharacteristics', 'sensitivity', 'specifity', 'classifierResult', 'AUC', 'VPpositive', 'VPnegative');
     
     stringres=strcat(num2str(indicesCcsSelected), ' - Descriptor: ', num2str(bestPCA));
     title(stringres)
-    saveas(h,['results\' lower(usedMethod) 'FeatureSelection_' name_t1 '_' name_t2 '.jpg'])
+    saveas(h,['results\' lower(usedMethod) 'FeatureSelection_' name_t1 '_' name_t2 '_' date '.jpg'])
 
     close all
 end
