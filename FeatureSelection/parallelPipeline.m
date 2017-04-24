def1327f9f295a2dfeebda2672ebabe0f8577159
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-04-24 14:59:46 +0200

    parallel pipele updated

--- FeatureSelection/parallelPipeline.m
+++ FeatureSelection/parallelPipeline.m
@@ -4,17 +4,46 @@ addpath(genpath('lib'))
 
 cd 'D:\Pablo\FeatureSelection\FeatureSelection\'
 
-%classifications(1, :) = {CharacteristicsGDDAAgainstControlInstabilityGDDUsingWeights1303, 'Alta', 'High', 'Rest'};
-classifications(1, :) = {CharacteristicsGDDAAgainstControlRiskGDDUsingWeights13032017, 'NoRisk', 'NoRisk', 'HighRisk', 'PCA'};
-classifications(2, :) = {CharacteristicsGDDAAgainstControlRiskGDDUsingWeights13032017, 'NoRisk', 'NoRisk', 'HighRisk', 'DA'};
-% classifications(2, :) = {CharacteristicsGDDAAgainstControlAge22032017, 'LowAge', 'LowAge', 'GreaterAge'};
-% classifications(3, :) = {CharacteristicsGDDAAgainstControlStage22032017, 'EarlyStage', 'EarlyStage', 'LateStage'};
-% classifications(4, :) = {CharacteristicsGDDAAgainstControlAP22032017, 'BetterAP', 'BetterAP', 'WorseAP'};
-% classifications(5, :) = {CharacteristicsGDDAAgainstControlNeupat22032017, 'BetterNeupat', 'BetterNeupat', 'WorseNeupat'};
+matrix2 = CharacteristicsGDDAAgainstControlToClassify24042017;
+matrixChar = table2array(matrix2(:, 8:end));
+emptyCells = cellfun(@(x) isequal('', x), matrix2.Inestabilidad);
 
+class1 = cellfun(@(x) isequal('Alta', x), matrix2.Inestabilidad);
+class4 = cellfun(@(x) isequal('Muy baja', x), matrix2.Inestabilidad);
+class3 = cellfun(@(x) isequal('Baja', x), matrix2.Inestabilidad);
+
+methods = {'DA'; 'PCA'};
+parfor i = 1:size(methods, 1)
+    FeatureSelection_2_cc(matrixChar(class1==0 & emptyCells, :), matrixChar(class1, :), 'Rest', 'High', methods{i});
+    FeatureSelection_2_cc(matrixChar(class4, :), matrixChar(class1, :), 'VeryLow', 'High', methods{i});
+    FeatureSelection_2_cc(matrixChar(class4, :), matrixChar(class3, :), 'VeryLow', 'Low', methods{i});
+end
+
+classifications(1, :) = { 'NoRisk', 'NoRisk', 'HighRisk', 'DA'};
+classifications(2, :) = { 'LowAge', 'LowAge', 'GreaterAge', 'DA'};
+classifications(3, :) = { 'EarlyStage', 'EarlyStage', 'LateStage', 'DA'};
+classifications(4, :) = { 'BetterAP', 'BetterAP', 'WorseAP', 'DA'};
+classifications(5, :) = { 'BetterNeupat', 'BetterNeupat', 'WorseNeupat', 'DA'};
+classifications(6, :) = { 'NoRisk', 'NoRisk', 'HighRisk', 'PCA'};
+classifications(7, :) = { 'LowAge', 'LowAge', 'GreaterAge', 'PCA'};
+classifications(8, :) = {'EarlyStage', 'EarlyStage', 'LateStage', 'PCA'};
+classifications(9, :) = {'BetterAP', 'BetterAP', 'WorseAP', 'PCA'};
+classifications(10, :) = {'BetterNeupat', 'BetterNeupat', 'WorseNeupat', 'PCA'};
+
+matrixChar = table2array(matrix2(:, 8:end));
 parfor i = 1:size(classifications, 1)
-    matrix2 = classifications{i, 1};
-    matrixChar = table2array(matrix2(:, 3:end));
-    class1 = cellfun(@(x) isequal(classifications{i, 2}, x), matrix2{:, 2});
-    FeatureSelection_2_cc(matrixChar(class1, :), matrixChar(class1==0, :), classifications{i, 3}, classifications{i, 4}, classifications{i, 5});
+    emptyCells = cellfun(@(x) isequal('', x), matrix2{:, i+2});
+    class1 = cellfun(@(x) isequal(classifications{i, 1}, x), matrix2{:, i+2});
+    FeatureSelection_2_cc(matrixChar(class1, :), matrixChar(class1==0 & ~emptyCells, :), classifications{i, 2}, classifications{i, 3}, classifications{i, 4});
+end
+
+%% Only VTN characteristics
+variableNames = strfind(lower(CharacteristicsGDDAAgainstControlToClassify24042017.Properties.VariableNames), 'vtn');
+headerNamesWithVTN = cellfun(@(x) isempty(x) == 0, variableNames);
+headerNamesWithVTN(:, 1:7) = [];
+matrixChar = table2array(matrix2(:, 8:end));
+parfor i = 1:size(classifications, 1)
+    emptyCells = cellfun(@(x) isequal('', x), matrix2{:, i+2});
+    class1 = cellfun(@(x) isequal(classifications{i, 1}, x), matrix2{:, i+2});
+    FeatureSelection_2_cc(matrixChar(class1, headerNamesWithVTN), matrixChar(class1==0 & ~emptyCells, headerNamesWithVTN), classifications{i, 2}, classifications{i, 3}, classifications{i, 4});
 end
