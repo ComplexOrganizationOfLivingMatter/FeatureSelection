73d2d69648097f6394de8de52f6f402089157ce3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-25 17:30:12 +0200

    exporting correct, but now we want to select a few characteristics

--- TestingLime/src/main.py
+++ TestingLime/src/main.py
@@ -12,8 +12,14 @@ from IPython.display import display, HTML
 
 imp=Imputer(missing_values='NaN',strategy='mean',axis=0)
 
+np.random.seed(1)
 allData = pd.read_csv("/home/ubuntu/vboxshare/Neuroblastoma/Results/graphletsCount/NuevosCasos/Analysis/Characteristics_GDDA_AgainstControl_ToClassify_24_04_2017.csv", sep=';')
 allDataAsMatrix = allData.as_matrix();
+selectedChar = np.array([47, 19, 17, 16, 18, 50, 26], dtype=np.intp);
+columNames = allData.columns.values[8:];
+columNames = columNames[selectedChar[:, np.newaxis]];
+columNames.tolist()
+#selectedChar = 1:59;
 onlyData = allDataAsMatrix[:, 8:]
 labels = allData['Risk']
 categoricalLabels = [0] * labels.size
@@ -23,13 +29,13 @@ onlyDataFinal = [];
 for numLabel in xrange(1, labels.size):
 	if labels[numLabel] == 'HR' or labels[numLabel] == 'UHR':
 		categoricalLabels[numLabel] = 1
-		onlyDataFinal.append(onlyData[numLabel])
+		onlyDataFinal.append(onlyData[numLabel, selectedChar[:, np.newaxis]])
 		categoricalLabelsFinal.append(categoricalLabels[numLabel])
 	elif labels[numLabel] != labels[numLabel]: #This is a NaN! wtf!?
 		categoricalLabels[numLabel] = -1
 	else:
 		onlyDataFinal.append(onlyData[numLabel])
-		categoricalLabelsFinal.append(categoricalLabels[numLabel])
+		categoricalLabelsFinal.append(categoricalLabels[numLabel, selectedChar[:, np.newaxis]])
 
 
 
@@ -38,10 +44,15 @@ rf = sklearn.ensemble.RandomForestClassifier(n_estimators=500)
 trainNoNaNs = imp.fit_transform(train)
 rf.fit(trainNoNaNs, labels_train)
 sklearn.metrics.accuracy_score(labels_test, rf.predict(test))
-explainer = lime.lime_tabular.LimeTabularExplainer(trainNoNaNs, feature_names=allData.columns.values.tolist(), class_names=['NoRisk', 'HighRisk'], discretize_continuous=True)
+explainer = lime.lime_tabular.LimeTabularExplainer(trainNoNaNs, feature_names=columNames, class_names=['NoRisk', 'HighRisk'], discretize_continuous=True)
 
-i = np.random.randint(0, len(test))
-exp = explainer.explain_instance(test[i], rf.predict_proba, num_features=59, top_labels=1)
 
-exp.show_in_notebook(show_table=True, show_all=False)
-exp.save_to_file('oi.html')
+for x in xrange(1,len(test)):
+	exp = explainer.explain_instance(test[x], rf.predict_proba, num_features=len(selectedChar))
+	#exp.show_in_notebook(show_table=True, show_all=True)
+	exp.save_to_file('../results/explanationOfTest' + str(x) +'.html')
+
+for x in xrange(1,len(train)):
+	exp = explainer.explain_instance(trainNoNaNs[x], rf.predict_proba, num_features=len(selectedChar))
+	#exp.show_in_notebook(show_table=True, show_all=True)
+	exp.save_to_file('../results/explanationOfTrain' + str(x) +'.html')
