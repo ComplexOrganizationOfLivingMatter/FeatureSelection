1b53cec1ca7144d3667b7906bece35f71234220d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-25 16:39:45 +0200

    seems to be working but not show the html

--- TestingLime/src/main.py
+++ TestingLime/src/main.py
@@ -7,30 +7,43 @@ import lime
 import lime.lime_tabular
 import pandas as pd
 import math
+from sklearn.preprocessing import Imputer
+from IPython.display import display, HTML
+
+imp=Imputer(missing_values='NaN',strategy='mean',axis=0)
 
 allData = pd.read_csv("/home/ubuntu/vboxshare/Neuroblastoma/Results/graphletsCount/NuevosCasos/Analysis/Characteristics_GDDA_AgainstControl_ToClassify_24_04_2017.csv", sep=';')
 allDataAsMatrix = allData.as_matrix();
 onlyData = allDataAsMatrix[:, 8:]
 labels = allData['Risk']
 categoricalLabels = [0] * labels.size
-usefulRows = [];
+categoricalLabelsFinal = [];
+onlyDataFinal = [];
 
 for numLabel in xrange(1, labels.size):
 	if labels[numLabel] == 'HR' or labels[numLabel] == 'UHR':
 		categoricalLabels[numLabel] = 1
-		usefulRows.append(numLabel)
+		onlyDataFinal.append(onlyData[numLabel])
+		categoricalLabelsFinal.append(categoricalLabels[numLabel])
 	elif labels[numLabel] != labels[numLabel]: #This is a NaN! wtf!?
 		categoricalLabels[numLabel] = -1
 	else:
-		usefulRows.append(numLabel)
-
-print(usefulRows)
-onlyData = onlyData[usefulRows, :]
-categoricalLabels = categoricalLabels[usefulRows]
-
-# train, test, labels_train, labels_test = sklearn.model_selection.train_test_split(onlyData, categoricalLabels, train_size=0.80)
+		onlyDataFinal.append(onlyData[numLabel])
+		categoricalLabelsFinal.append(categoricalLabels[numLabel])
 
 
+
+train, test, labels_train, labels_test = sklearn.model_selection.train_test_split(onlyDataFinal, categoricalLabelsFinal, train_size=0.80)
+rf = sklearn.ensemble.RandomForestClassifier(n_estimators=500)
+trainNoNaNs = imp.fit_transform(train)
+rf.fit(trainNoNaNs, labels_train)
+sklearn.metrics.accuracy_score(labels_test, rf.predict(test))
+explainer = lime.lime_tabular.LimeTabularExplainer(trainNoNaNs, feature_names=allData.columns.values.tolist(), class_names=['NoRisk', 'HighRisk'], discretize_continuous=True)
+
+i = np.random.randint(0, len(test))
+exp = explainer.explain_instance(test[i], rf.predict_proba, num_features=2, top_labels=1)
+
+display(HTML(exp.show_in_notebook(show_table=True, show_all=False)))
 # data = np.genfromtxt('/Users/marcotcr/phd/datasets/mushroom/agaricus-lepiota.data', delimiter=',', dtype='<U20')
 # labels = data[:,0]
 # le= sklearn.preprocessing.LabelEncoder()
