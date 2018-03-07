e892f1fb6cc26f8a92a35881b1b26c9e29c76715
Pablo Vicente Munuera <pvicente1@us.es>
2018-03-07 10:33:19 +0100

    new function to check the best combinations

---
+++ FeatureSelection/src/testCombinationsOfFeatures.m
@@ -0,0 +1,14 @@
+function [ results ] = testCombinationsOfFeatures( featuresCombinations, labelsUsed, matrixChar, columnNames, condition)
+%TESTCOMBINATIONSOFFEATURES Summary of this function goes here
+%   Detailed explanation goes here
+
+    results = cell(length(featuresCombinations), 4);
+    for numCombination = 1:length(featuresCombinations)
+        usedChars = featuresCombinations{numCombination};
+        ftc = FeatureSelectionClass(labelsUsed, matrixChar, 'LogisticRegression', usedChars, columnNames, condition);
+        [ goodness, projection, sensitivity, specificity] = getHowGoodAreTheseCharacteristics(ftc.matrixAllCases(:, usedChars), grp2idx(categorical(ftc.labels)), -1, 'LogisticRegression');
+        results(numCombination, :) = { goodness, projection, sensitivity, specificity};
+    end
+
+end
+
