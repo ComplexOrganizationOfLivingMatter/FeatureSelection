cfec938e90dd71be4aec375be8541740fdf7cbc9
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-18 17:44:33 +0200

    confusionmatrix

--- FeatureSelection/firthLogisticRegression.R
+++ FeatureSelection/firthLogisticRegression.R
@@ -4,3 +4,14 @@ y <- as.matrix(predictors)
 ( m2 <- brglm(y ~ x[, 1] + x[, 2] + x[, 3] + x[, 4] + x[, 5] + x[, 6] + x[, 7], family = binomial(logit)) )
 summary(m2)
 confint.default(m2)
+
+model <- m2
+data <- characteristics
+
+#https://gist.github.com/ryanwitt/2911560
+prediction <- ifelse(predict(model, data, type='response') > 0.5, TRUE, FALSE)
+confusion  <- table(prediction, as.logical(model$y))
+confusion  <- cbind(confusion, c(1 - confusion[1,1]/(confusion[1,1]+confusion[2,1]), 1 - confusion[2,2]/(confusion[2,2]+confusion[1,2])))
+confusion  <- as.data.frame(confusion)
+names(confusion) <- c('FALSE', 'TRUE', 'class.error')
+confusion
