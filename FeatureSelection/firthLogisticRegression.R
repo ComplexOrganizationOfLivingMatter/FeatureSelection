cf891e72a22597d0eebfda649c99569c09d633f6
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-18 17:56:07 +0200

    other types of glms

--- FeatureSelection/firthLogisticRegression.R
+++ FeatureSelection/firthLogisticRegression.R
@@ -4,10 +4,12 @@ characteristics <- file;
 x <- as.matrix(characteristics)
 y <- as.matrix(predictors)
 ( m2 <- brglm(y ~ x[, 1] + x[, 2] + x[, 3] + x[, 4] + x[, 5] + x[, 6] + x[, 7], family = binomial(logit)) )
+m3 <- logistf(y ~ x[, 1] + x[, 2] + x[, 3] + x[, 4] + x[, 5] + x[, 6] + x[, 7], family = binomial(logit)) 
+mOriginal <- glm(y ~ x[, 1] + x[, 2] + x[, 3] + x[, 4] + x[, 5] + x[, 6] + x[, 7], family = binomial(logit))
 summary(m2)
 confint.default(m2)
 
-model <- m2
+model <- mOriginal
 data <- characteristics
 
 #https://gist.github.com/ryanwitt/2911560
