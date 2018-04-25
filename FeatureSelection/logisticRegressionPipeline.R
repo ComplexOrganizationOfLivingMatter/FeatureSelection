f693bc2be98955f112117a6eed8e20881d769dae
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-25 12:15:56 +0200

    formulate the steps

--- FeatureSelection/logisticRegressionPipeline.R
+++ FeatureSelection/logisticRegressionPipeline.R
@@ -51,7 +51,7 @@ significantCharacteristics <- characteristicsWithoutClinicVTN[,univariateAnalysi
 
 significantAndClinicChars <- cbind(significantCharacteristics, characteristicsOnlyClinic)
 #library(leaps)
-colNamesOfFormula <- paste(names(signifcantAndClinicChars), collapse='` + `' );
+colNamesOfFormula <- paste(names(significantAndClinicChars), collapse='` + `' );
 formula <- paste("RiskCalculatedDicotomized ~ `", colNamesOfFormula, "`", sep='')
 #leapsResults <- regsubsets(RiskCalculatedDicotomized ~ `VTN++ - meanPercentageOfFibrePerFilledCell` + `VTN++ - stdPercentageOfFibrePerFilledCell` + `VTN++ - meanQuantityOfBranchesFilledPerCell` + `VTN++ - eulerNumberPerFilledCell` + `INRG_EDAD` + `INRG_ESTADIO` + `INRG_HistoCat` + `INRG_HistoDif` + `INRG_SCA` + `INRG_MYCN` + `INRG_PLOIDIA` + `INRG_11q`, data = initialInfoDicotomized, nbest= 1, nvmax = NULL, force.in = NULL, force.out = NULL, method = "exhaustive")
 #summary.leaps<-summary(leapsResults)
@@ -79,12 +79,27 @@ layout(matrix(1:2, ncol = 2))
 res.legend <-
   subsets(res.best.logistic, statistic="adjr2", legend = FALSE, min.size = 5, main = "Adjusted R^2")
 
+library(glmulti)
 glmulti.logistic.out <-
-  glmulti(formula, data = initialInfoDicotomized,
+  glmulti(as.formula(formula), data = initialInfoDicotomized,
           level = 1,               # No interaction considered
           method = "h",            # Exhaustive approach
           crit = "aic",            # AIC as criteria
-          confsetsize = 10,         # Keep 5 best models
+          confsetsize = 5,         # Keep 5 best models
           plotty = F, report = F,  # No plot or interim reports
           fitfunction = "glm",     # glm function
           family = binomial) 
+
+
+## Forth step: Check collinearity and confusion/interaction
+
+# Collinearity
+colNamesOfFormula <- paste(colnames(res.best.logistic$BestModel$model[, 2:length(res.best.logistic$BestModel$model)]), collapse='` + `' );
+finalFormula <- as.formula(paste("RiskCalculatedDicotomized ~ `", colNamesOfFormula, "`", sep=''))
+vif(glm(finalFormula, data=initialInfoDicotomized, family = binomial(logit)))
+
+
+# Confusion and Interaction
+xtabs(finalFormula, data=initialInfoDicotomized)
+
+## Fifth step: Calculate the relative importance of each predictor within the model
