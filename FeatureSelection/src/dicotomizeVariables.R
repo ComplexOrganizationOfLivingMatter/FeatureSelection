bb2aeaff29f3668f708b8005d2511c58cb61877f
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-25 16:54:54 +0200

    dicotomize variables by several methods done

--- FeatureSelection/src/dicotomizeVariables.R
+++ FeatureSelection/src/dicotomizeVariables.R
@@ -2,22 +2,21 @@ dicotomizeVariables <- function(initialInfo, initialIndex, columnStatus, healthy
   
   
   initialInfoDicotomized <- initialInfo
+  columnsUsed <- initialIndex:(length(initialInfo[1,])-8)
   
   if (cutoffMethod == 'Median'){
-    ## Option 2: Median
-    mediansPerColumn <- apply(as.matrix(characteristicsWithoutClinic), 2, median)
-    df %>% mutate(a = cut(a, breaks = quantile(a, probs = seq(0, 1, 0.2))))
+    initialInfoDicotomized <- mapply(initialInfo[, columnsUsed], FUN=function(column)
+      as.numeric(column < median(column)))
     
   } else if (cutoffMethod == '3rdQuartile'){
-    ## Option 3: third quartile
-    xs=quantile(actualVariable, c(0,1/3,2/3,1))
-    xs[1]=xs[1]-.00005
-    df1 <- df %>% mutate(category=cut(a, breaks=xs, labels=c(0, 1)))
-  } else {
-    ## Option 1: Younden's Index
+    initialInfoDicotomized <- mapply(initialInfo[, columnsUsed], FUN=function(column)
+      as.numeric(column < quantile(column, 0.75)))
+    
+  } else {## Option using ROC curve
+    
     library(OptimalCutpoints)
     cutoffValues = c();
-    for (numColumn in initialIndex:(length(initialInfo[1,])-8)) {
+    for (numColumn in columnsUsed) {
       #You have several methods to use instead of Youden
       youdenValue <- optimal.cutpoints(X = colnames(initialInfo[,numColumn]), status=columnStatus, tag.healthy=healthyTag, methods=cutoffMethod, data = as.data.frame(initialInfo))
       cutoffValues[numColumn] <- youdenValue$MaxSpSe$Global$optimal.cutoff$cutoff
