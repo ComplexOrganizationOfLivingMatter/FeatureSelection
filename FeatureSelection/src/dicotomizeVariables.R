7a309000a1527d60b7e3908085ed41c994454a17
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-25 16:32:01 +0200

    several options for dicotomize variables

--- FeatureSelection/src/dicotomizeVariables.R
+++ FeatureSelection/src/dicotomizeVariables.R
@@ -3,27 +3,32 @@ dicotomizeVariables <- function(initialInfo, initialIndex, columnStatus, healthy
   
   initialInfoDicotomized <- initialInfo
   
-  ## Option 1: Younden's Index
-  library(OptimalCutpoints)
-  cutoffValues = c();
-  for (numColumn in initialIndex:(length(initialInfo[1,])-8)) {
-    #You have several methods to use instead of Youden
-    youdenValue <- optimal.cutpoints(X = colnames(initialInfo[,numColumn]), status=columnStatus, tag.healthy=healthyTag, methods=cutoffMethod, data = as.data.frame(initialInfo))
-    cutoffValues[numColumn] <- youdenValue$Youden$`Global`$optimal.cutoff$`cutoff`
+  if (cutoffMethod == 'Median'){
+    ## Option 2: Median
+    mediansPerColumn <- apply(as.matrix(characteristicsWithoutClinic), 2, median)
+    df %>% mutate(a = cut(a, breaks = quantile(a, probs = seq(0, 1, 0.2))))
     
-    #Perform the cutoff
-    initialInfoDicotomized[,numColumn] <- as.numeric(initialInfo[,numColumn] > cutoffValues[numColumn]);
+  } else if (cutoffMethod == '3rdQuartile'){
+    ## Option 3: third quartile
+    xs=quantile(actualVariable, c(0,1/3,2/3,1))
+    xs[1]=xs[1]-.00005
+    df1 <- df %>% mutate(category=cut(a, breaks=xs, labels=c(0, 1)))
+  } else {
+    ## Option 1: Younden's Index
+    library(OptimalCutpoints)
+    cutoffValues = c();
+    for (numColumn in initialIndex:(length(initialInfo[1,])-8)) {
+      #You have several methods to use instead of Youden
+      youdenValue <- optimal.cutpoints(X = colnames(initialInfo[,numColumn]), status=columnStatus, tag.healthy=healthyTag, methods=cutoffMethod, data = as.data.frame(initialInfo))
+      cutoffValues[numColumn] <- youdenValue$MaxSpSe$Global$optimal.cutoff$cutoff
+      
+      #print(numColumn)
+      #print(cutoffValues[numColumn])
+      
+      #Perform the cutoff
+      initialInfoDicotomized[,numColumn] <- as.numeric(initialInfo[,numColumn] > cutoffValues[numColumn]);
+    }
   }
-  
-  
-  # ## Option 2: Median
-  # mediansPerColumn <- apply(as.matrix(characteristicsWithoutClinic), 2, median) 
-  # df %>% mutate(a = cut(a, breaks = quantile(a, probs = seq(0, 1, 0.2))))
-  # 
-  # ## Option 3: third quartile
-  # xs=quantile(actualVariable, c(0,1/3,2/3,1))
-  # xs[1]=xs[1]-.00005
-  # df1 <- df %>% mutate(category=cut(a, breaks=xs, labels=c(0, 1)))
   
   return (initialInfoDicotomized)
 }
